55b2deb7644b1832b66dfb41a34494f7d5261bd9
Alex Neuenkirk <alex.neuenkirk@gmail.com>
2016-05-05 19:35:07 -0400

    Added testing for class GenderedLocution.

--- LocutionTester.js
+++ LocutionTester.js
@@ -1,17 +1,21 @@
 define(["require", "exports", './AUNLG', 'cif'], function (require, exports, AUNLG, cif) {
     "use strict";
     function test() {
-        var testSting = "That's so %specialized(nice)%!";
+        var testSpecialized = "That's so %specialized(nice)%!";
+        var testRandom = "This is %random(wicked, wretched, awesome)%!";
+        var testGendered = "Clayton is having trouble keeping %gendered(his/her/their)% partner happy.";
         var loadResult = cif.init();
         var rawSchema = cif.loadFile("etsData/schema.json");
         var schema = cif.loadSocialStructure(rawSchema);
         var rawCast = cif.loadFile("etsData/cast.json");
         var cast = cif.addCharacters(rawCast);
-        var locutions = AUNLG.preprocessDialogue(testSting);
+        var locutions = AUNLG.preprocessDialogue(testSpecialized);
         var testBindings = {
             "x": "Clayton"
         };
         var i;
+        var locutions = AUNLG.preprocessDialogue(testGendered);
+        console.log("The raw dialogue string was: " + testGendered);
         for (i = 0; i < locutions.length; i++) {
             console.log(locutions[i].renderText("x", testBindings));
         }
--- LocutionTester.ts
+++ LocutionTester.ts
@@ -2,10 +2,12 @@ import AUNLG = require('./AUNLG');
 import cif = require('cif');
 
 export function test(): void {
-   var testSting:string = "That's so %specialized(nice)%!";
-   // var testSting:string = "This is %random(wicked, wretched, awesome)%!"
+   var testSpecialized:string = "That's so %specialized(nice)%!";
+   var testRandom:string = "This is %random(wicked, wretched, awesome)%!";
+   // Is the x needed in the raw dialogue string? It seems that it is only needed for renderText.
+   var testGendered:string = "Clayton is having trouble keeping %gendered(his/her/their)% partner happy.";
 
-   //Init CiF
+   // Init CiF.
    var loadResult:any = cif.init();
 
    //Load in our schema, cast, triggerRules and volitionRules, and actions.
@@ -15,16 +17,28 @@ export function test(): void {
    var rawCast:any = cif.loadFile("etsData/cast.json");
    var cast:Array<string> = cif.addCharacters(rawCast);
 
-
    /* DEBUG/TEST */
-   var locutions: Array<AUNLG.Locution> = AUNLG.preprocessDialogue(testSting);
+   var locutions: Array<AUNLG.Locution> = AUNLG.preprocessDialogue(testSpecialized);
 
    var testBindings:any = {
      "x":"Clayton"
    }
 
    var i: number;
+   // console.log("THIS IS THE OUTPUT FOR THE SPECIALIZED LOCUTION");
+   // console.log("The raw dialogue string was: " + testSpecialized);
+   // for (i = 0; i < locutions.length; i++) {
+   //    console.log(locutions[i].renderText("x", testBindings));
+   // }
+   // var locutions: Array<AUNLG.Locution> = AUNLG.preprocessDialogue(testRandom);
+   // console.log("THIS IS THE OUTPUT FOR THE RANDOM LOCUTION");
+   // console.log("The raw dialogue string was: " + testRandom);
+   // for (i = 0; i < locutions.length; i++) {
+   //     console.log(locutions[i].renderText(undefined, undefined));
+   // }
+   var locutions:Array<AUNLG.Locution> = AUNLG.preprocessDialogue(testGendered);
+   console.log("The raw dialogue string was: " + testGendered);
    for (i = 0; i < locutions.length; i++) {
-      console.log(locutions[i].renderText("x", testBindings));
+       console.log(locutions[i].renderText("x", testBindings));
    }
 }
