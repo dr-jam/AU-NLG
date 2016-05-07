033d61a0ae48559226a951e29fe7f49924c45d84
Alex Neuenkirk <alex.neuenkirk@gmail.com>
2016-05-06 21:49:42 -0400

    Reworked tester file for an easier testing environment.

--- LocutionTester.js
+++ LocutionTester.js
@@ -1,24 +1,44 @@
 define(["require", "exports", './AUNLG', 'cif'], function (require, exports, AUNLG, cif) {
     "use strict";
     function test() {
-        var testSpecialized = "That's so %specialized(nice)%!";
-        var testRandom = "This is %random(wicked, wretched, awesome)%!";
-        var testGendered = "Clayton is having trouble keeping %gendered(his/her/their)% partner happy.";
+        var testStrings = [
+            "That's so %specialized(nice)%!",
+            "This is %random(wicked, wretched, awesome)%!",
+            "Clayton is having trouble keeping %gendered(his/her/their)% partner happy."
+        ];
+        var testBindings = {
+            "x": "Clayton"
+        };
         var loadResult = cif.init();
         var rawSchema = cif.loadFile("etsData/schema.json");
         var schema = cif.loadSocialStructure(rawSchema);
         var rawCast = cif.loadFile("etsData/cast.json");
         var cast = cif.addCharacters(rawCast);
-        var locutions = AUNLG.preprocessDialogue(testSpecialized);
-        var testBindings = {
-            "x": "Clayton"
-        };
         var i;
-        var locutions = AUNLG.preprocessDialogue(testGendered);
-        console.log("The raw dialogue string was: " + testGendered);
-        for (i = 0; i < locutions.length; i++) {
-            console.log(locutions[i].renderText("x", testBindings));
+        for (i = 0; i < testStrings.length; i++) {
+            console.log("\n");
+            testDialogueString(testStrings[i], "x", testBindings);
         }
     }
     exports.test = test;
+    function getPreprocessedDialogue(pTestString) {
+        return AUNLG.preprocessDialogue(pTestString);
+    }
+    function getRenderedTexts(pLocutions, pSpeaker, pBindings) {
+        var renderedTexts = [];
+        var i;
+        for (i = 0; i < pLocutions.length; i++) {
+            renderedTexts.push(pLocutions[i].renderText(pSpeaker, pBindings));
+        }
+        return renderedTexts;
+    }
+    function concatTextsArray(pRenderedTexts) {
+        return pRenderedTexts.join("");
+    }
+    function testDialogueString(pDialogue, pSpeaker, pBindings) {
+        console.log(pDialogue);
+        var locutions = getPreprocessedDialogue(pDialogue);
+        var locutionStrings = getRenderedTexts(locutions, pSpeaker, pBindings);
+        console.log(concatTextsArray(locutionStrings));
+    }
 });
--- LocutionTester.ts
+++ LocutionTester.ts
@@ -2,43 +2,65 @@ import AUNLG = require('./AUNLG');
 import cif = require('cif');
 
 export function test(): void {
-   var testSpecialized:string = "That's so %specialized(nice)%!";
-   var testRandom:string = "This is %random(wicked, wretched, awesome)%!";
-   // Is the x needed in the raw dialogue string? It seems that it is only needed for renderText.
-   var testGendered:string = "Clayton is having trouble keeping %gendered(his/her/their)% partner happy.";
+   var testStrings:Array<string> = [
+       "That's so %specialized(nice)%!",
+       "This is %random(wicked, wretched, awesome)%!",
+       "Clayton is having trouble keeping %gendered(his/her/their)% partner happy."
+   ];
+   var testBindings:any = {
+     "x":"Clayton"
+   };
 
    // Init CiF.
    var loadResult:any = cif.init();
-
    //Load in our schema, cast, triggerRules and volitionRules, and actions.
    var rawSchema:any = cif.loadFile("etsData/schema.json");
    var schema:any = cif.loadSocialStructure(rawSchema);
-
    var rawCast:any = cif.loadFile("etsData/cast.json");
    var cast:Array<string> = cif.addCharacters(rawCast);
 
-   /* DEBUG/TEST */
-   var locutions: Array<AUNLG.Locution> = AUNLG.preprocessDialogue(testSpecialized);
-
-   var testBindings:any = {
-     "x":"Clayton"
-   }
-
+   // Output the tests.
    var i: number;
-   // console.log("THIS IS THE OUTPUT FOR THE SPECIALIZED LOCUTION");
-   // console.log("The raw dialogue string was: " + testSpecialized);
-   // for (i = 0; i < locutions.length; i++) {
-   //    console.log(locutions[i].renderText("x", testBindings));
-   // }
-   // var locutions: Array<AUNLG.Locution> = AUNLG.preprocessDialogue(testRandom);
-   // console.log("THIS IS THE OUTPUT FOR THE RANDOM LOCUTION");
-   // console.log("The raw dialogue string was: " + testRandom);
-   // for (i = 0; i < locutions.length; i++) {
-   //     console.log(locutions[i].renderText(undefined, undefined));
-   // }
-   var locutions:Array<AUNLG.Locution> = AUNLG.preprocessDialogue(testGendered);
-   console.log("The raw dialogue string was: " + testGendered);
-   for (i = 0; i < locutions.length; i++) {
-       console.log(locutions[i].renderText("x", testBindings));
+   for (i = 0; i < testStrings.length; i++) {
+       console.log("\n");
+       testDialogueString(testStrings[i], "x", testBindings);
    }
+}
+
+
+/**
+ * getPreprocessedDialogue
+ *      Preprocessed a raw dialogue string into an array of type
+ *      AUNLG.Locution.
+ *
+ * @param  {string} pTestString:string
+ *      The Raw dialogue string that should be preprocessed.
+ * @return {Array<AUNLG.Locution>}
+ *      An array of instantiated derivations of AUNGL.Locution.
+ */
+function getPreprocessedDialogue(pTestString:string):Array<AUNLG.Locution> {
+    return AUNLG.preprocessDialogue(pTestString);
+}
+
+
+function getRenderedTexts(pLocutions:Array<AUNLG.Locution>, pSpeaker:string, pBindings:any):Array<string> {
+    var renderedTexts:Array<string> = [];
+    var i:number;
+    for (i = 0; i < pLocutions.length; i++) {
+        renderedTexts.push(pLocutions[i].renderText(pSpeaker, pBindings));
+    }
+    return renderedTexts;
+}
+
+
+function concatTextsArray(pRenderedTexts:Array<string>):string {
+    return pRenderedTexts.join("");
+}
+
+
+function testDialogueString(pDialogue:string, pSpeaker:string, pBindings:any) {
+    console.log(pDialogue);
+    var locutions:Array<AUNLG.Locution> = getPreprocessedDialogue(pDialogue);
+    var locutionStrings = getRenderedTexts(locutions, pSpeaker, pBindings);
+    console.log(concatTextsArray(locutionStrings));
 }
